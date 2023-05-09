71fdf395eab8e1960aebc9b1c7eaf721f4a6db7c
Thomas Böhler <t.boehler@kunbus.com>
2023-05-09 16:07:43 +0200

    Add tests for video and audio output
    Roughly, outputs are sent to the framebuffer and ALSA default device to
    test the functionality of video and audio output.
    A more precise description of how these tests work is given in the
    README.
    
    Signed-off-by: Thomas Böhler <t.boehler@kunbus.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,150 @@
-fn main() {
-    println!("Hello, World!");
+//! Test video and audio output by showing coloured frames on the framebuffer and playing back a
+//! siren over the default ALSA audio device.
+
+use alsa::pcm::{Access, Format, HwParams, State, PCM};
+use alsa::{Direction, ValueOr};
+use anyhow::{bail, Context};
+use framebuffer::{Framebuffer, KdMode};
+use std::{thread, time};
+
+const TTY: &str = "/dev/tty1";
+const FB: &str = "/dev/fb0";
+
+// how long a frame is displayed in seconds
+const FRAME_LENGTH: u64 = 1;
+// amount of time one audio sample should play
+const AUDIO_LENGTH: u64 = 1;
+
+fn frame_set_color(frame: &mut [u8], color: (u8, u8, u8)) {
+    for pixel in &mut frame.chunks_mut(4) {
+        pixel[0] = color.2;
+        pixel[1] = color.1;
+        pixel[2] = color.0;
+        pixel[3] = 0xff;
+    }
+}
+
+fn main() -> anyhow::Result<()> {
+    if let Some(a) = std::env::args().nth(1) {
+        if a == "--version" {
+            let name = env!("CARGO_PKG_NAME");
+            let version = env!("CARGO_PKG_VERSION");
+            eprintln!("{name}: {version}");
+            std::process::exit(0);
+        }
+    }
+
+    let frame_thread = thread::spawn(|| -> anyhow::Result<()> { frame() });
+    let siren_thread = thread::spawn(|| -> anyhow::Result<()> { siren() });
+
+    if let Ok(frame_thread_status) = frame_thread.join() {
+        // propagate error that might have happened in the thread
+        frame_thread_status?;
+    } else {
+        bail!("Unable to join frame_thread");
+    };
+    if let Ok(siren_thread_status) = siren_thread.join() {
+        siren_thread_status?;
+    } else {
+        bail!("Unable to join siren_thread");
+    };
+
+    Ok(())
+}
+
+/// Display 3 frames, each being shown for `FRAME_LENGTH` seconds. The frames will alternate in
+/// Red, Green, and Blue.
+fn frame() -> anyhow::Result<()> {
+    let mut framebuffer = Framebuffer::new(FB)?;
+
+    let w = framebuffer.var_screen_info.xres;
+    let h = framebuffer.var_screen_info.yres;
+    let line_length = framebuffer.fix_screen_info.line_length;
+    let bytespp = framebuffer.var_screen_info.bits_per_pixel / 8;
+
+    println!("w: {w}; h: {h}; line_length: {line_length}; bpp: {bytespp}");
+
+    let mut frame = vec![0u8; (line_length * h) as usize];
+
+    //Disable text mode for tty1
+    Framebuffer::set_kd_mode_ex(TTY, KdMode::Graphics)
+        .context("Unable to disable text mode on framebuffer")?;
+
+    frame_set_color(&mut frame, (0xff, 0, 0));
+    framebuffer.write_frame(&frame);
+    thread::sleep(time::Duration::from_secs(FRAME_LENGTH));
+
+    frame_set_color(&mut frame, (0, 0xff, 0));
+    framebuffer.write_frame(&frame);
+    thread::sleep(time::Duration::from_secs(FRAME_LENGTH));
+
+    frame_set_color(&mut frame, (0, 0, 0xff));
+    framebuffer.write_frame(&frame);
+    thread::sleep(time::Duration::from_secs(FRAME_LENGTH));
+
+    //Reenable text mode in current tty
+    Framebuffer::set_kd_mode_ex(TTY, KdMode::Text)
+        .context("Unable to enable text mode on framebuffer")?;
+
+    Ok(())
+}
+
+/// Sound a siren over the default ALSA device. The siren consists of 3 tones, each being played
+/// back for `AUDIO_LENGTH` seconds.
+fn siren() -> anyhow::Result<()> {
+    // Open default playback device
+    let pcm = PCM::new("default", Direction::Playback, false)?;
+    let info = pcm.info()?;
+    let name = info.get_name()?;
+    println!("Device: {name}");
+
+    // Set hardware parameters: 44100 Hz / Mono / 16 bit
+    let hwp = HwParams::any(&pcm)?;
+    hwp.set_channels(1)?;
+    hwp.set_rate(44100, ValueOr::Nearest)?;
+    hwp.set_format(Format::s16())?;
+    hwp.set_access(Access::RWInterleaved)?;
+    pcm.hw_params(&hwp)?;
+    let io = pcm.io_i16()?;
+
+    // Make sure we don't start the stream too early
+    let hwp = pcm.hw_params_current()?;
+    let swp = pcm.sw_params_current()?;
+    swp.set_start_threshold(hwp.get_buffer_size()?)?;
+    pcm.sw_params(&swp)?;
+
+    // Make a sine wave
+    let mut buf = [0i16; 1024];
+    for (i, a) in buf.iter_mut().enumerate() {
+        *a = ((i as f32 * 2.0 * ::std::f32::consts::PI / 128.0).sin() * 8192.0) as i16
+    }
+
+    // Play it back for AUDIO_LENGTH seconds.
+    for _ in 0..AUDIO_LENGTH * 44100 / 1024 {
+        assert_eq!(io.writei(&buf[..])?, 1024);
+    }
+
+    for (i, a) in buf.iter_mut().enumerate() {
+        *a = ((i as f32 * 4.0 * ::std::f32::consts::PI / 128.0).sin() * 8192.0) as i16
+    }
+
+    for _ in 0..AUDIO_LENGTH * 44100 / 1024 {
+        assert_eq!(io.writei(&buf[..])?, 1024);
+    }
+
+    for (i, a) in buf.iter_mut().enumerate() {
+        *a = ((i as f32 * 6.0 * ::std::f32::consts::PI / 128.0).sin() * 8192.0) as i16
+    }
+
+    for _ in 0..AUDIO_LENGTH * 44100 / 1024 {
+        assert_eq!(io.writei(&buf[..])?, 1024);
+    }
+
+    if pcm.state() != State::Running {
+        pcm.start()?
+    };
+    // Wait for the stream to finish playback.
+    pcm.drain()?;
+
+    Ok(())
 }
